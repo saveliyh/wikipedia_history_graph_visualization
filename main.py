d95730c416cd2dbd01e7c850973826ddc3b829e3
Saveliy Khlebnov <savel.khlebnov@yandex.ru>
2025-02-28 23:21:48 +0300

    add database

--- main.py
+++ main.py
@@ -1,12 +1,50 @@
-from parse import get_links_from_page
-
-
+from parse import get_links_from_page, get_page_data
 import json
+import asyncio
+from skytable_py import Config, Query
+
+c = Config("root", "661caa49-74a5-44", host="localhost", port=2003)
+
+
+async def main():
+    db = None
+    try:
+        db = await c.connect()
+        pages = await get_links_from_page(titles="Derek Kolstad")
+        page = await get_page_data(titles="Derek Kolstad")
+        # init space
+        assert (
+            await db.run_simple_query(Query("create space if not exists pages"))
+        ).is_empty()
+
+        # init model
+        assert (
+            await db.run_simple_query(
+                Query(
+                    """create model if not exists pages.page(
+                      id: uint16,
+                      title: string,
+                      connected: list {type: uint16}
+                      )"""
+                )
+            )
+        ).is_empty()
+
+        # insert data
+
+        assert await db.run_simple_query(
+            Query("""insert into pages.page(id, title, connected) values (?, ?, ?)"""),
+            page["pageid"],
+            page["title"],
+            [link["pageid"] for link in pages],
+        )
+
+    except Exception as e:
+        print(e)
+    finally:
+        if db:
+            await db.close()
+
 
 if __name__ == "__main__":
-
-    pages = get_links_from_page("Derek Kolstad")
-
-    with open("data.txt", "w") as w:
-        for json_data in pages:
-            print(json.dumps(json_data, indent=4, sort_keys=True), file=w)
+    asyncio.run(main())
--- parse.py
+++ parse.py
@@ -1,13 +1,31 @@
 import requests as re
 
 
-def get_links_from_page(page_title="Main Page"):
+async def get_page_data(**kwargs):
     params = {
         "action": "query",
         "format": "json",
-        "titles": page_title,
+    }
+    params.update(kwargs)
+    response = re.get(
+        url="https://en.wikipedia.org/w/api.php",
+        params=params,
+    ).json()
+    if "error" in response:
+        raise Exception(response["error"])
+    elif "warnings" in response:
+        print(response["warnings"])
+    elif "query" in response:
+        return response["query"]["pages"].values()[0]
+
+
+async def get_links_from_page(**kwargs):
+    params = {
+        "action": "query",
+        "format": "json",
         "generator": "links",
     }
+    params.update(kwargs)
     connected_pages = []
     last_continue = {}
     while True:
@@ -30,5 +48,5 @@ def get_links_from_page(page_title="Main Page"):
             break
 
         last_continue = response["continue"]
-        print(response)
+        # TODO: add logging
     return connected_pages
