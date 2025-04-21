f958fb6c24494283b22e02637a1d4b4d8a8c1e96
Saveliy Khlebnov <savel.khlebnov@yandex.ru>
2025-04-21 10:32:32 +0300

    api

--- api/src/db.rs
+++ api/src/db.rs
@@ -1,19 +1,20 @@
 
 use skytable::{
-    query, response::RList, Config, Response
+    query, response::{RList, Rows}, Config, Response
 };
 
 #[derive(Response)]
 #[derive(Clone, PartialEq, Debug)] // we just do these for the assert (they are not needed)
 pub struct Page{
-    pub pageid: u64,
+    pub id: u64,
     pub title: Vec<u8>,
     pub connected: skytable::response::RList<u64>,
 }
 
 #[derive(Response)]
 pub struct Metadata{
-    page_ids: RList<u64>,
+    id: u8,
+    pages: RList<u64>,
 }
 
 pub fn connect(host: &str, port: u16, username: &str, password: &str) -> Result<skytable::Connection, skytable::error::Error> {
@@ -22,12 +23,16 @@ pub fn connect(host: &str, port: u16, username: &str, password: &str) -> Result<
 }
 
 pub fn get_all_nodes(db: &mut skytable::Connection) -> Result<Vec<u64>, skytable::error::Error> {
-    let query = query!("select page_ids from pages.metadata");
-    let metadata: Metadata = db.query_parse(&query)?;
-    Ok(metadata.page_ids.into_values())
+    let query = query!("select all * from pages.metadata limit ?", 10u64);
+    
+    let metadata: Rows<Metadata> = db.query_parse(&query)?;
+    // println!("{} {:?}", metadata[0].id, metadata[0].pages);
+    // println!("{:?}", pages);
+    
+    Ok(metadata[0].pages.clone().into_values())
 }
 
 pub fn get_page(db: &mut skytable::Connection, pageid: u64) -> Result<Page, skytable::error::Error> {
-    let query = query!("select * from pages.page where pageid = ?", pageid);
+    let query = query!("select * from pages.page where id = ?", pageid);
     db.query_parse::<Page>(&query)
 }
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,12 +1,35 @@
 use db::get_all_nodes;
 use rocket::serde::json::Json;
 use rocket::serde::Serialize;
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::Header;
+use rocket::Request;
+use rocket::Response;
 
 mod db;
 #[macro_use] 
 extern crate rocket;
 extern crate serde_derive;
 extern crate toml;
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[derive(Serialize)]
 struct PageResponse{
@@ -15,6 +38,11 @@ struct PageResponse{
     connected: Vec<u64>,
 }
 
+#[derive(Serialize)]
+struct LinkResponse{
+    source: u64,
+    target: u64,
+}
 
 #[get("/all_nodes")]
 fn all_nodes_handler() -> Json<Vec<u64>>{
@@ -40,8 +68,9 @@ fn get_page_handler(pageid: u64) -> Json<PageResponse>{
         db = db::connect("localhost", 2003, "root", "dev_password_will_be_changed");
         println!("wait for db");
     }
+
     match db::get_page(&mut db.unwrap(), pageid){
-        Ok(value) => {return Json(PageResponse { pageid: value.pageid,
+        Ok(value) => {return Json(PageResponse { pageid: value.id,
              title: String::from_utf8(value.title).unwrap(), 
              connected: value.connected.into_values()
         });},
@@ -52,10 +81,42 @@ fn get_page_handler(pageid: u64) -> Json<PageResponse>{
     }
 }
 
+#[get("/all_links")]
+fn all_links_handler() -> Json<Vec<LinkResponse>>{
+    let mut db = db::connect("localhost", 2003, "root", "dev_password_will_be_changed");
+    while db.is_err() {
+        db = db::connect("localhost", 2003, "root", "dev_password_will_be_changed");
+        println!("wait for db");
+    }
+    let nodes; 
+    let mut db = db.unwrap();
+    match db::get_all_nodes(&mut db) {
+        Ok(value) => {nodes = value;},
+        Err(e) => {
+            println!("Cannot get link list from db: {}", e);
+            return Json(vec![]);
+        }
+    }
+    let mut links    = vec![];
+    for pageid in nodes.iter(){
+        match db::get_page(&mut db, *pageid){
+            Ok(value) => {
+                links.extend(value.connected.iter().filter(|x| nodes.contains(*x)).map(|x| LinkResponse{source: *pageid, target: *x}));
+            },
+            Err(e) => {
+                println!("Cannot get node {} from db: {}", *pageid, e);
+            }
+        }
+    }
+    Json(links)
+}
 
 #[launch]
 fn rocket() -> _ {
     
-    rocket::build().mount("/all_nodes", routes![all_nodes_handler])
-                    .mount("/page/<page_id>", routes![get_page_handler])
+
+    rocket::build().mount("/", routes![all_nodes_handler])
+                    .mount("/", routes![get_page_handler])
+                    .mount("/", routes![all_links_handler])
+                    .attach(CORS)
 }
